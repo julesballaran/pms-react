33292e020e5f084b8a83700e41aaa4dc16c6365e
Jules <jules.ballaran@boom.camp>
2019-07-04 10:21:03 +0800

    update

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import Paper from '@material-ui/core/Paper';
+import bookImg from '../../img/book.png'
 export default function Books(){
   return (
-    <h1>Books</h1>
+    <Paper style={{width: 100}}>
+      <div style={{margin: '0 auto'}}>
+        <img src={bookImg} alt='' style={{height: 50}}/>
+      </div>
+    </Paper>
   )
 }
