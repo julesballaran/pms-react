37f7baaf9dbdb9f3608e1c21d28d17b3d9a6ff13
Jules Ballaran <julesballara@gmail.com>
2019-07-07 18:09:36 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
-
+import background from './img/grid2bg.jpg'
 import './App.css';
 import Header from './components/Header/Header'
 import Sidenav from './components/Sidenav/Sidenav'
@@ -9,7 +9,7 @@ import Routes from './routes'
 function App() {
   return (
     <BrowserRouter>
-      <div style={{display: 'flex',}}>
+      <div style={{display: 'flex', backgroundImage: `url(${background})`}}>
         <Sidenav />
         <div style={{display: 'flex', flexDirection: 'column', maxWidth: 'calc(100% - 240px)', minWidth: 'calc(100% - 240px)',}}>
         <Header />
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,12 +1,111 @@
-import React from 'react'
-import Paper from '@material-ui/core/Paper';
-import bookImg from '../../img/book.png'
+import React, {useState} from 'react'
+import {
+  Paper,
+  Modal,
+  Select,
+  MenuItem,
+  InputLabel,
+  TextField,
+  Button,
+  Grid,
+}
+  from '@material-ui/core/';
+import Book from '@material-ui/icons/Book';
+
+const modalStyle = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexDirection: 'column',
+  alignItems: 'center',
+  background: 'white',
+  outline: 'none',
+  height: 300,
+  width: 350,
+}
+
+const btnStyle = {
+  width: 150,
+  height: 35,
+  display: 'flex',
+  margin: '15px 10px 0 auto',
+  fontSize: 15,
+  fontWeight: 'bolder',
+  border: '1px solid black',
+}
+
+
 export default function Books(){
+
+  const [open, setOpen] = useState(false)
+  const [type, setType] = useState('')
+  const [bookName, setBookName] = useState('')
+  const [bookNo, setBookNo] = useState('')
+  
   return (
-    <Paper style={{width: 100}}>
-      <div style={{margin: '0 auto'}}>
-        <img src={bookImg} alt='' style={{height: 50}}/>
-      </div>
-    </Paper>
+    <React.Fragment>
+      <Button 
+        style={btnStyle} 
+        onClick={() => setOpen(true)}
+      >Add Book
+      </Button>
+      <Grid
+        container
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+      <Paper className='book-style'>
+        <Book style={{fontSize: 120,}}/>
+        <h3>Book 1</h3>
+        <p>200 entries</p>
+      </Paper>
+      <Paper className='book-style'></Paper>
+      <Paper className='book-style'></Paper>
+      <Paper className='book-style'></Paper>
+      <Paper className='book-style'></Paper>
+      </Grid>
+
+      <Modal 
+        open={open}
+        onClose={() => setOpen(false)}
+        style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+      > 
+        <div style={modalStyle}>
+          <TextField
+            style={{width: '90%'}}
+            required
+            label="Book Name"
+            value={bookName}
+            margin="normal"
+            onChange={e => setBookName(e.target.value)}
+          />
+          <TextField
+            style={{width: '90%'}}
+            required
+            label="Book Number"
+            type="number"
+            value={bookNo}
+            margin="normal"
+            onChange={e => setBookNo(e.target.value)}
+          />
+          <InputLabel style={{width: '90%'}}>Type: 
+            <Select
+              style={{width: '100%'}}
+              value={type}
+              onChange={e => setType(e.target.value)}
+              label="type"
+            >
+              <MenuItem style={{minWidth: '90%'}} value='Baptismal'>Baptismal</MenuItem>
+              <MenuItem style={{minWidth: '90%'}} value='Confirmation'>Confirmation</MenuItem>
+              <MenuItem style={{minWidth: '90%'}} value='Death'>Death</MenuItem>
+              <MenuItem style={{minWidth: '90%'}} value='Marriage'>Marriage</MenuItem>
+            </Select>
+          </InputLabel>
+          <Button variant="contained">
+            ADD
+          </Button>
+        </div>
+      </Modal>
+    </React.Fragment>
   )
 }
